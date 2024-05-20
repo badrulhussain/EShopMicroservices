47cb2d5f7ae245d6b79e99dec69960b9afe63973
Badrul <47890161+badrulhussain@users.noreply.github.com>
2024-05-20 21:41:19 +0100

    Created Product Model and Feature class for Product create. Added the MediatR package.

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Catalog.API.Products.CreateProduct
+{
+
+    public record CreateProductCommand(string Name, List<string> Catagory, string Description, string ImageFime, decimal Price)
+        : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductHandler
+    {
+    }
+}
