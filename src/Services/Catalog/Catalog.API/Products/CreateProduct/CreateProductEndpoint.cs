daf262332f7f6d23e2fa9f8f6adba4e58ee2231b
Badrul <47890161+badrulhussain@users.noreply.github.com>
2024-05-23 23:07:14 +0100

    Added the Request and Response records and Carter library

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,8 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
+    public record CreateProductRequest(string Name, List<string> Catagory, string Description, string ImageFime, decimal Price);
+    public record CreateProductResponse(Guid Id);
+
     public class CreateProductEndpoint
     {
     }
