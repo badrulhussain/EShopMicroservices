e178322ed14376c5bb40bb3bd68c9ac8e1fa581f
Badrul <47890161+badrulhussain@users.noreply.github.com>
2024-06-13 02:55:49 +0100

    Created the Get Product Handler to access the DB and created the Get Products Endpoin for the minimal API. Used Request and Response records for the API's and Used Command and Query records for the DB handlers.

---
+++ src/Services/Catalog/Catalog.API/Products/GetProduts/GetProductsEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.GetProduts
+{
+    //public record GetProductRequest();
+    public record GetProductResponse(IEnumerable<Product> Products);
+
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var reuslt = await sender.Send(new GetProductsQuery());
+
+                var reposne = reuslt.Adapt<GetProductResponse>();
+
+                return Results.Ok(reposne);
+            })
+                .WithName("GetProducts")
+                .Produces<GetProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Products")
+                .WithDescription("Get Products");
+        }
+    }
+}
